ff0407eb85fcee9a2677666323f0c8e6a9376283
vcarpetto <victor@materia.io>
2016-01-27 09:00:11 -0500

    Update patterndb.ps1

--- nxlog-main/patterndb.ps1
+++ nxlog-main/patterndb.ps1
@@ -1,13 +1,14 @@
 # Script name:   nxlog patterndb helpder
 # Created on:    12/31/2015
 # Author:        Victor Carpetto
+# History:       v1.0, 12/31/2015, VC
+
 
 ## Define Variables
-$Path = "C:\Program Files (x86)\nxlog\conf\patterndb.xml"
+$PatternDBpath = "C:\Program Files (x86)\nxlog\conf\patterndb.xml"
+$regexUPN = "(.*)@(.*)"
 $global:idx = 1
 
-## Regex for UPN split / replace
-$regexUPN = "(.*)@(.*)"
 
 ## Build AD Array of Objects
 ipmo ActiveDirectory
@@ -16,13 +17,13 @@ $Domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
 $Domain.Forest.Domains |
     % {
         $prefix = (Get-ADdomain $($_.name)).NetBIOSName + "\"
-        $users += (Get-ADuser -filter {enabled -eq $true} -server "$($_.name):389") |
+        $users += (Get-ADuser -filter {enabled -eq $true} -server "$($_.name)") |
             select @{name="idx";expression = {($global:idx++)}},@{Name="domsam"; Expression = {($prefix + $_.samaccountname).tolower()}},@{Name="upn";Expression = {[regex]::replace($_.userprincipalname, $regexUPN, '$2\$1').tolower()}}
     }
 
 
 ## Write PatternDB
-$xmlWriter = New-Object System.XMl.XmlTextWriter($Path,$Null)
+$xmlWriter = New-Object System.XMl.XmlTextWriter($PatternDBpath,$Null)
 $xmlWriter.Formatting = 'Indented'
 $xmlWriter.Indentation = 1
 $xmlWriter.IndentChar = "`t"
@@ -49,7 +50,7 @@ $users | % {
                 $xmlWriter.WriteElementString('value',$_.upn)
             $xmlWriter.WriteEndElement()
         $xmlWriter.WriteEndElement()
-        $xmlWriter.WriteElementString('exec','if $UPN =~ /.*/ log_info("FOUND UPN: " + $UPN + " Source:" + $IpAddress);')
+		$xmlWriter.WriteElementString('exec','if $UPN =~ /.*/ log_info("FOUND UPN: " + $UPN + " Source:" + $IpAddress);')
     $xmlWriter.WriteEndElement()
 }
 $xmlWriter.WriteEndElement()
@@ -60,9 +61,8 @@ $xmlWriter.Close()
 
 ## Cycle nxLog service and purge logs
 Stop-Service nxlog -Verbose
-rm L:\logs\eventlog.* -Verbose
-rm "C:\Program Files (x86)\nxlog\data\nxlog.log" -Verbose
+Clear-Content 'C:\Program Files (x86)\nxlog\data\nxlog.log'
+## add in better service restart logic
+Start-Sleep 5
 Start-Service nxlog -Verbose
 
-
-$xmlWriter.WriteElementString('exec','if $UPN =~ /.*/ log_info("FOUND UPN: " + $UPN + " Source:" + $IpAddress);')
